4a03a85f289aa88c96398c8e542c21ef1dc5d84d
nikos <nikoskoutr@gmail.com>
2016-04-26 20:20:54 +0300

    More eigenface! Turned to javacv

--- app/src/main/java/com/apps/ktr/facerec/RecognizeFaceHelperTask.java
+++ app/src/main/java/com/apps/ktr/facerec/RecognizeFaceHelperTask.java
@@ -8,14 +8,24 @@ import android.os.Environment;
 import android.util.Log;
 import android.widget.ImageView;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.CvType;
-import org.opencv.android.Utils;
-import org.bytedeco.javacpp.openc
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_face;
+import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
+import org.bytedeco.javacpp.opencv_imgcodecs;
+import org.bytedeco.javacv.AndroidFrameConverter;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ref.WeakReference;
+import java.nio.IntBuffer;
 import java.util.Vector;
 
 
@@ -35,7 +45,6 @@ public class RecognizeFaceHelperTask extends AsyncTask<String, Void, Bitmap> {
     private String mCurrentPhotoPath;
     private int mAlgorithm;
     private Context mContext;
-    private String mCsvLocation = Environment.getExternalStorageDirectory() + "/" + ROOT + "/images.csv";
     private WeakReference<ImageView> wR;
 
     public RecognizeFaceHelperTask(String photoPath, int algorithmNumber, Context c, ImageView iV){
@@ -63,17 +72,23 @@ public class RecognizeFaceHelperTask extends AsyncTask<String, Void, Bitmap> {
                 Log.e(TAG, "Interrupted exception while sleeping detect face thread" + e.toString());
             }
         }
-        Bitmap bmp32 = input.copy(Bitmap.Config.ARGB_8888, true);
-        Mat mat;
-        mat = norm(bmp32);
-        Log.e(TAG, mat.channels() + "");
-        bmp32 = Bitmap.createBitmap(mat.width(), mat.height(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(mat, bmp32);
-        //return bmp32;
+//        Bitmap bmp32 = input.copy(Bitmap.Config.ARGB_8888, true);
+//        Mat mat = new Mat();
+//        mat = norm(bmp32);
+//        Log.e(TAG, mat.channels() + "");
+//        bmp32 = matToBitmap(mat);
 
-        Vector<Mat> images = new Vector<>();
-        Vector<Integer> labels = new Vector<>();
-        readCsv(mCsvLocation, images, labels);
+        String csvLocation = Environment.getExternalStorageDirectory() + "/" + ROOT + "/images.csv";
+        int lines = countLines(csvLocation);
+        opencv_core.MatVector images = new opencv_core.MatVector(lines);
+        Mat labels = new Mat(lines, 1, opencv_core.CV_32SC1);
+
+        readCsv(csvLocation, images, labels);
+        int height = images.get(0).rows();
+//
+        FaceRecognizer model = opencv_face.createEigenFaceRecognizer();
+        model.
+        model.train(images, new Mat(1, labels.length, labels));
     }
 
     @Override
@@ -85,30 +100,98 @@ public class RecognizeFaceHelperTask extends AsyncTask<String, Void, Bitmap> {
     }
 
     private Mat norm (Bitmap b) {
-        Mat ret = new Mat();
-        Utils.bitmapToMat(b, ret);
+        Mat ret = bitmapToMat(b);
         Mat returningMat = new Mat();
 
         switch (ret.channels()) {
-            case 1:
-                Core.normalize(ret, returningMat, 0.0, 255.0, Core.NORM_MINMAX, CvType.CV_8UC1);
+            case 1: {
+                opencv_core.normalize(ret, returningMat, 0.0, 255.0, opencv_core.NORM_MINMAX, opencv_core.CV_8UC1, null);
                 return returningMat;
-            case 2:
-                Core.normalize(ret, returningMat, 0.0, 255.0, Core.NORM_MINMAX, CvType.CV_8UC2);
+            }
+            case 2: {
+                opencv_core.normalize(ret, returningMat, 0.0, 255.0, opencv_core.NORM_MINMAX, opencv_core.CV_8UC2, null);
                 return returningMat;
-            case 3:
-                Core.normalize(ret, returningMat, 0.0, 255.0, Core.NORM_MINMAX, CvType.CV_8UC3);
+            }
+            case 3: {
+                opencv_core.normalize(ret, returningMat, 0.0, 255.0, opencv_core.NORM_MINMAX, opencv_core.CV_8UC3, null);
                 return returningMat;
-            case 4:
-                Core.normalize(ret, returningMat, 0.0, 255.0, Core.NORM_MINMAX, CvType.CV_8UC4);
+            }
+            case 4: {
+                opencv_core.normalize(ret, returningMat, 0.0, 255.0, opencv_core.NORM_MINMAX, opencv_core.CV_8UC4, null);
                 return returningMat;
-            default:
+            }
+            default: {
                 return ret;
+            }
         }
     }
 
-    private void readCsv(String location, Vector<Mat> images, Vector<Integer> labels) {
-        File rootFolder = new File(location);
+    private void readCsv(String location, opencv_core.MatVector images, Mat labels) {
+        File csv = new File(location);
+        try {
+            BufferedReader bR = new BufferedReader(new FileReader(csv));
+            while(!bR.ready()){
+                try {
+                    wait(1000);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, "Error waiting thread for reader: " + e.getMessage());
+                }
+            }
+            String line = bR.readLine();
+            IntBuffer labelsBuf = labels.getIntBuffer();
+            int counter = 0;
+            while (line != null) {
+                int index = line.indexOf(";");
+                if (index != -1) {
+                    Mat image = opencv_imgcodecs.imread(line.substring(0, index));
+                    Integer label = Integer.parseInt(line.substring(index + 1));
+                    images.put(counter, image);
+                    labelsBuf.put(counter, label);
+                    counter++;
+                }
+                line = bR.readLine();
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error reading csv file: " + e.getMessage());
+        }
+    }
+
+    private Mat bitmapToMat(Bitmap bmp) {
+        OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
+        AndroidFrameConverter toFrame = new AndroidFrameConverter();
+        Frame frame = toFrame.convert(bmp);
+        Mat mat = converter.convertToMat(frame);
+        return mat;
+    }
+
+    private Bitmap matToBitmap (Mat mat) {
+        OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
+        AndroidFrameConverter toBitmap = new AndroidFrameConverter();
+        Frame frame = converter.convert(mat);
+        Bitmap bmp = toBitmap.convert(frame);
+        return bmp;
+    }
+
+    public int countLines(String filename) {
+        try {
+            InputStream is = new BufferedInputStream(new FileInputStream(filename));
+            byte[] c = new byte[1024];
+            int count = 0;
+            int readChars = 0;
+            boolean empty = true;
+            while ((readChars = is.read(c)) != -1) {
+                empty = false;
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n') {
+                        ++count;
+                    }
+                }
+            }
+            return (count == 0 && !empty) ? 1 : count;
+        } catch (IOException e) {
+            Log.e(TAG, "Error counting lines");
+        }
+        return 0;
     }
 
 
